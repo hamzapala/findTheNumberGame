b3a9490079842e6ec245a862f8d06d38bf6be2ce
HAMZA PALA <hamzapala48@gmail.com>
2022-02-15 02:34:08 +0300

    done

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ let closestHight = 100;
 function findTheNumber() {
     if (realResult == myGuess.value){
         result.style.display = "none";
-        attempt.style.display = "none"
-        congrats.style.display = "block"
+        attempt.style.display = "none";
+        congrats.style.display = "block";
         congrats.innerHTML = "Congratulations you won!!!";
         congrats.style.color = "green";
         congrats.style.fontSize = "5rem";
